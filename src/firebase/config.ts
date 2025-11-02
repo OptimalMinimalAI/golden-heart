2bccae457d680f39472368aaf5f414cdc73c165b
Brandon <optimalminimalai@gmail.com>
2025-11-02 10:04:49 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-2893901069-da885",
+  "appId": "1:428623975233:web:21419d49c122aa52c307c0",
+  "apiKey": "AIzaSyCGQ-ND3yhVe4o3cw8rqiPaW-Fe-F2U0-o",
+  "authDomain": "studio-2893901069-da885.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "428623975233"
+};
